e9e804d50cfcc2d167eff377511500ccd8303fc9
Wolfgang <wolfgang_xu@paradise-soft.com.tw>
2020-09-18 15:23:12 +0800

    feat: compare trees

--- concurrency/equivalent-binary-tree.go
+++ concurrency/equivalent-binary-tree.go
@@ -6,6 +6,8 @@ import (
 	"golang.org/x/tour/tree"
 )
 
+const capacity = 10
+
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
@@ -21,17 +23,47 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int, capacity)
+	go Walk(t1, ch1)
+
+	ch2 := make(chan int, capacity)
+	go Walk(t2, ch2)
+
+	for i := 0; i < capacity; i++ {
+		if <-ch1 != <-ch2 {
+			return false
+		}
+	}
+
 	return true
 }
 
 func main() {
 	tree1 := tree.New(1)
+	traverse(tree1)
+	tree2 := tree.New(1)
+	traverse(tree2)
 
-	ch := make(chan int, 10)
-	go Walk(tree1, ch)
+	result := Same(tree1, tree2)
+	fmt.Println("---")
+	fmt.Printf("Are above trees same as each other? %v\n", result)
 
-	fmt.Println(tree1)
-	for i := 0; i < cap(ch); i++ {
+	tree2 = tree.New(2)
+
+	result = Same(tree1, tree2)
+	fmt.Println("---")
+	fmt.Println("Tree: ", tree1)
+	fmt.Println("Tree: ", tree2)
+	fmt.Printf("Are above trees same as each other? %v\n", result)
+}
+
+func traverse(tree *tree.Tree) {
+	ch := make(chan int, capacity)
+	go Walk(tree, ch)
+
+	fmt.Println("Tree: ", tree)
+	fmt.Println("Traversal:")
+	for i := 0; i < capacity; i++ {
 		fmt.Println(<-ch)
 	}
 }
